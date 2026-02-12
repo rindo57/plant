ac07b1a4db21c2b869146bd7b5b1c4dbe576450b
rindo57 <77073061+rindo57@users.noreply.github.com>
2026-02-12 11:41:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
 # Configure Gemini
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel('gemini-1.5-flash')
-
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 def get_gemini_response(image_path):
     img = Image.open(image_path)
     prompt = """
